9391b4511a871f4130712cbef05f3fe8472b77d4
dimar55 <dimargalvan55@gmail.com>
2022-11-25 10:50:26 -0500

    update

--- src/controllers/loteCtrl.js
+++ src/controllers/loteCtrl.js
@@ -2,7 +2,7 @@ const pool = require('../models/repository_postgre');
 
 exports.create = async (req, res) => {
     const {precio_entrada, precio_venta, cantidad_lote, fecha_vencimiento, cedula_pro, cedula_usu} = req.body;
-    const response = await pool.query(`INSERT INTO lote (precio_entrada, precio_venta, cantidad_lote, fecha_vencimiento, cedula_pro, cedula_usu) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id_lote`, [precio_entrada, precio_venta, cantidad_lote, fecha_entrada, fecha_vencimiento, cedula_pro, cedula_usu]);
+    const response = await pool.query(`INSERT INTO lote (precio_entrada, precio_venta, cantidad_lote, fecha_vencimiento, cedula_pro, cedula_usu) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id_lote`, [precio_entrada, precio_venta, cantidad_lote, fecha_vencimiento, cedula_pro, cedula_usu]);
     res.status(201).json({
         success: true,
         message: 'Created',

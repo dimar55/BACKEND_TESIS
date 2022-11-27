6ce1b4e132161b5df870cd78a093546f9a452c37
Schied <davidcelis682@gmail.com>
2022-11-27 09:21:14 -0500

    Update

--- src/controllers/saldoCtrl.js
+++ src/controllers/saldoCtrl.js
@@ -49,3 +49,12 @@ exports.getByFilter = async (req, res) => {
     }
 }
 
+exports.updateSaldo = async (req, res) => {
+    try {
+        const { id_saldo, estado_saldo, saldo } = req.body;
+        const response = await pool.query(`UPDATE saldo SET estado_saldo = '${estado_saldo}', saldo = ${saldo} WHERE id_saldo = ${id_saldo}`);
+        res.status(204).send({ success: true, body: response.rows});
+    } catch (error) {
+        res.status(500).send({success: false, body: error});
+    }
+}
--- src/routes/saldo.js
+++ src/routes/saldo.js
@@ -5,5 +5,5 @@ const saldoCtrl = require('../controllers/saldoCtrl');
 router.get('/', saldoCtrl.getAll);
 router.post('/', saldoCtrl.create);
 router.post('/filter', saldoCtrl.getByFilter);
-
+router.put('/', saldoCtrl.updateSaldo);
 module.exports = router;

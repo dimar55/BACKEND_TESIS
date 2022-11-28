6f4f92e3fca7d93186ff974d1afb82bb37b6aae4
Schied <davidcelis682@gmail.com>
2022-11-27 20:17:36 -0500

    Update

--- src/controllers/ventaCtrl.js
+++ src/controllers/ventaCtrl.js
@@ -35,6 +35,32 @@ exports.getAll = async (req, res) => {
     }
 }
 
+exports.getByDate = async (req, res) => {
+    try {
+        const {Dateini, Datefin} = req.body;
+        const ventas = await pool.query(`SELECT id_venta, total_venta FROM venta WHERE fecha_venta >= '${Dateini}' AND fecha_venta <= '${Datefin}'`);
+        const response = await pool.query(`SELECT  venta.id_venta, to_char(venta.fecha_venta, 'YYYY-MON-DD') as fecha_venta, producto.nombre_product FROM venta_producto INNER JOIN producto ON producto.id_product = venta_producto.id_product INNER JOIN venta ON venta.id_venta = venta_producto.id_venta`);
+        let resp = [];
+        for (let index = 0; index < ventas.rows.length; index++) {
+            const element = ventas.rows[index];
+            let prods = response.rows.filter(ele => ele.id_venta == element.id_venta);
+            if(prods.length>0){
+                const venta = {
+                    id_venta: element.id_venta,
+                    total_venta: element.total_venta,
+                    fecha_venta: prods[0].fecha_venta,
+                    prods: obtenerNombres(prods)
+                }
+                resp.push(venta)
+            }
+            
+        }
+        res.status(200).send({success: true, body: resp})
+    } catch (error) {
+        res.status(500).send({ success: false, message: 'venta.getAll', body: error})
+    }
+}
+
 exports.insertarVentaProducto = async (req, res) => {
     const {productos} = req.body;
     const ventap = [];
--- src/routes/venta.js
+++ src/routes/venta.js
@@ -3,6 +3,7 @@ const router = Router();
 const ventaCtrl = require('../controllers/ventaCtrl');
 
 router.get('/', ventaCtrl.getAll);
+router.post('/filtro', ventaCtrl.getByDate);
 router.post('/', ventaCtrl.create, ventaCtrl.insertarVentaProducto);
 
 module.exports = router;
